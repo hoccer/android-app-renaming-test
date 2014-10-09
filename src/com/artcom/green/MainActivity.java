3d884ae2d28e5b4ca69f4e544f2babd3660be8b8
Guido Lorenz <guido.lorenz@artcom.de>
2014-10-09 10:59:39 +0200

    Import credentials from Hoccer XO using Intent

--- src/com/artcom/green/MainActivity.java
+++ src/com/artcom/green/MainActivity.java
@@ -1,7 +1,12 @@
 package com.artcom.green;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
     /**
@@ -11,5 +16,28 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button button = (Button)findViewById(R.id.btn_import);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                ComponentName component = new ComponentName("com.hoccer.xo.release", "com.hoccer.xo.android.activity.DataExportActivity");
+                intent.setComponent(component);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                String credentialsJson = data.getStringExtra("credentialsJson");
+                Log.i("GREEN", "got credentials: " + credentialsJson);
+            } else {
+                Log.i("GREEN", "did not get credentials");
+            }
+        }
     }
 }
